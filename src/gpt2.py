91b293ea5b783daff9adf986f5b0d0cc0bc668f1
fcoverdugo <fcoverdugoa@underpost.net>
2024-09-23 11:40:02 -0300

    add src util module

--- src/gpt2.py
+++ src/gpt2.py
@@ -1,17 +1,26 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import sys
+from util import index_exists
 
 model = AutoModelForCausalLM.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
-prompt = "GPT2 is a model developed by OpenAI."
+prompt = (
+    "GPT2 is a model developed by OpenAI."
+    if not index_exists(sys.argv, 1)
+    else sys.argv[1]
+)
+
+# python gpt2.py "hi gpt"
+print("prompt: ", prompt)
 
 input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 
 gen_tokens = model.generate(
     input_ids,
     do_sample=True,
-    temperature=0.9,
+    temperature=0.7,
     max_length=100,
 )
 
-print(tokenizer.batch_decode(gen_tokens))
+print("output: ", tokenizer.batch_decode(gen_tokens)[0])
---
+++ src/util.py
@@ -0,0 +1,2 @@
+def index_exists(ls, i):
+    return (0 <= i < len(ls)) or (-len(ls) <= i < 0)
