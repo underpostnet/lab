8f7c55039cf838469f62aaaffc55f69ebbc5383b
fcoverdugo <fcoverdugoa@underpost.net>
2025-01-15 17:19:51 -0300

    sd sprite advance

--- src/sd-huggingface.py
+++ src/sd-huggingface.py
@@ -1,13 +1,18 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
+# https://huggingface.co/docs/diffusers/v0.14.0/en/stable_diffusion
+
 model_id = "runwayml/stable-diffusion-v1-5"
 
-prompt = "portrait photo of a old warrior chief"
+
+prompt = "generates top view plain game asset image, cyber cowboy, as a pixel art, retro, 8-bit, clasic gba pokemon"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 
 pipe = pipe.to("cuda")
+
+# guidance scale, inference steps, noise schedule.
 
 generator = torch.Generator("cuda").manual_seed(0)
 
