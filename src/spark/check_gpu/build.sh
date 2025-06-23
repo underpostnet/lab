521f263f573a483351d4c20f46c61fe0e3d18ba0
underpostnet <fcoverdugoa@underpost.net>
2025-06-23 08:15:20 -0400

    Fix build order spark cuda app

--- src/spark/check_gpu/build.sh
+++ src/spark/check_gpu/build.sh
@@ -1,6 +1,6 @@
 sudo podman pull nvidia/cuda:11.8.0-cudnn8-runtime-ubuntu22.04
 sudo rm -rf ../lab/spark-cuda_latest.tar
 node bin dockerfile-image-build --path ../lab/src/spark/check_gpu --image-name=spark-cuda:latest --image-path=../lab --podman-save --kind-load --no-cache
-kubectl apply -f ./manifests/deployment/spark/cuda-test.yaml
 kubectl delete SparkApplication spark-gpu-test
+kubectl apply -f ./manifests/deployment/spark/cuda-test.yaml
 kubectl get SparkApplication -o wide -w
