24347090dec38128422329e13a3de1cbffe24859
underpostnet <fcoverdugoa@underpost.net>
2025-06-23 08:03:45 -0400

    Update check gpu spark builder

--- src/spark/check_gpu/build.sh
+++ src/spark/check_gpu/build.sh
@@ -2,4 +2,5 @@ sudo podman pull nvidia/cuda:11.8.0-cudnn8-runtime-ubuntu22.04
 sudo rm -rf ../lab/spark-cuda_latest.tar
 node bin dockerfile-image-build --path ../lab/src/spark/check_gpu --image-name=spark-cuda:latest --image-path=../lab --podman-save --kind-load --no-cache
 kubectl apply -f ./manifests/deployment/spark/cuda-test.yaml
-kubectl get pods -w -o wide
+kubectl delete SparkApplication spark-gpu-test
+kubectl get SparkApplication -o wide -w
