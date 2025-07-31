1482524ce6e477319e3baabd608dde56922c1cdd
underpostnet <fcoverdugoa@underpost.net>
2025-07-30 21:24:14 -0400

    Update gpu test sparkapplication

--- src/tf_gpu_test/build.sh
+++ src/tf_gpu_test/build.sh
@@ -6,11 +6,11 @@ IMAGE_NAME_FULL="${IMAGE_NAME}:${PROJECT_VERSION}"
 
 cd ${PROJECT_DIR}
 
-sudo podman pull nvidia/cuda:12.9.0-base-ubuntu24.04
+sudo podman pull docker.io/tensorflow/tensorflow:2.13.0-gpu
 
 sudo rm -rf ${PROJECT_DIR}/${IMAGE_NAME}_${PROJECT_VERSION}.tar
 
-underpost dockerfile-image-build --path ${PROJECT_DIR} --image-name=${IMAGE_NAME_FULL} --image-path=${PROJECT_DIR} --podman-save --kubeadm-load --no-cache
+underpost dockerfile-image-build --path ${PROJECT_DIR} --image-name=${IMAGE_NAME_FULL} --image-path=${PROJECT_DIR} --podman-save --kubeadm-load --reset
 
 kubectl apply -f ./${IMAGE_NAME}.yaml
 
